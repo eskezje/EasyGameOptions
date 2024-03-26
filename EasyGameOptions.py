a766582507a45300eb131ab5103947a2156cfb41
eskezje <114604325+eskezje@users.noreply.github.com>
2024-03-26 12:13:01 +0100

    QOL
    Added the option to what you want included in the script.
    
    Make GUI more bearable.
    
    You now need to pick at least 1 option to use explore

--- EasyGameOptions.py
+++ EasyGameOptions.py
@@ -103,31 +103,45 @@ def apply_DSCP(game_path, enable_DSCP):
     else:
         messagebox.showinfo("Info", "DSCP is not enabled.")
 
-def generate_script(game_path, formatted_mask_hex):
+def generate_script(game_path, formatted_mask_hex, enable_high_priority, enable_affinities):
     game_dir, game_exe = os.path.split(game_path)
     game_name, _ = os.path.splitext(os.path.basename(game_exe))  # Extracting just the executable name from the full path
 
     script_content = (
         "@echo off\n"
-        "cd /d \"%s\"\n"
-        "start /affinity %s /high %s\n"  # Remove the quotes around %s
-    ) % (game_dir, formatted_mask_hex, game_exe)
+        "cd /d \"%s\"\n"  # Change directory to the game directory
+        % game_dir +
+        "start "  # Start the game process
+    )
+    if enable_affinities:
+        script_content += "/affinity %s " % formatted_mask_hex  # Add affinity option if selected
+    if enable_high_priority:
+        script_content += "/high "  # Add high priority option if selected
+    script_content += "%s\n" % game_exe  # Start the game executable
 
-    script_file = os.path.join(os.path.expanduser("~"), "Desktop", "%s_high_affinities.bat" % game_name)
+    script_file = os.path.join(os.path.expanduser("~"), "Desktop", "%s_script.bat" % game_name)
     with open(script_file, "w") as f:
         f.write(script_content)
 
     messagebox.showinfo("Success", "Script saved to: %s" % script_file)
 
-# Function to allow users to select the game executable
 def explore():
     apply_priority_var = enable_priority_var.get()
     apply_FSE_var = enable_FSE_var.get()
-    apply_DSCP_var = enable_DSCP_var.get()  # Get the value of the DSCP checkbox
+    apply_DSCP_var = enable_DSCP_var.get()
+    enable_high_priority = enable_high_priority_var.get()  # Get the value of the /high checkbox
+    enable_affinities = enable_affinities_var.get()  # Get the value of the /affinities checkbox
+
+    # Check if at least one option is selected
+    if not (apply_priority_var or apply_FSE_var or apply_DSCP_var or enable_high_priority or enable_affinities):
+        messagebox.showwarning("Warning", "Pick at least 1 option to use Explore.")
+        return
+
     game_path = select_game_file()
     if not game_path:
         messagebox.showwarning("Warning", "No file selected.")
         return
+
     num_cores = get_number_of_cores() - 1
     mask = 0
     for i in range(1, num_cores + 1):
@@ -137,9 +151,9 @@ def explore():
 
     if apply_priority_var:
         apply_priority(game_path, apply_priority_var)
-    generate_script(game_path, formatted_mask_hex)
+    generate_script(game_path, formatted_mask_hex, enable_high_priority, enable_affinities)
     apply_FSE(game_path, apply_FSE_var)
-    apply_DSCP(game_path, apply_DSCP_var)  # Apply DSCP settings
+    apply_DSCP(game_path, apply_DSCP_var)
 
 def revert_FSE():
     game_path = select_game_file()
@@ -205,6 +219,10 @@ enable_DSCP_var = IntVar()
 enable_priority_var = IntVar()
 enable_FSE_var = IntVar()
 
+# Define variables to track whether /high and /affinities should be added to the script
+enable_high_priority_var = IntVar()
+enable_affinities_var = IntVar()
+
 # Create and position the label prompting the user to select the game executable
 label = Label(root, text="Press the button to select the game executable:")
 label.pack()
@@ -213,8 +231,20 @@ label.pack()
 explore_button = Button(root, text="Explore", command=explore)
 explore_button.pack()
 
+# Create and position the frame to contain options for generating the script
+generating_script_frame = ttk.LabelFrame(root, text="Generating Script", labelanchor='n')
+generating_script_frame.pack(pady=10)
+
+# Create and position the checkbox for enabling /high option
+high_checkbutton = Checkbutton(generating_script_frame, text="Add /high option", variable=enable_high_priority_var)
+high_checkbutton.pack()
+
+# Create and position the checkbox for enabling /affinities option
+affinities_checkbutton = Checkbutton(generating_script_frame, text="Add /affinities option", variable=enable_affinities_var)
+affinities_checkbutton.pack()
+
 # Create and position the frame to contain high priority checkbox and revert button
-priority_frame = ttk.LabelFrame(root, text="Add high priority to registry?")
+priority_frame = ttk.LabelFrame(root, text="Add high priority to registry?", labelanchor='n')
 priority_frame.pack(pady=10)
 
 # Create and position the checkbox for enabling high priority
@@ -226,7 +256,7 @@ revert_button = Button(priority_frame, text="Revert", command=revert)
 revert_button.pack(side="right")
 
 # Create and position the frame to contain FSE checkbox
-fse_frame = ttk.LabelFrame(root, text="Enable Full-Screen Exclusive (FSE)?")
+fse_frame = ttk.LabelFrame(root, text="Enable Full-Screen Exclusive (FSE)?", labelanchor='n')
 fse_frame.pack(pady=10)
 
 # Create and position the checkbox for enabling FSE
@@ -238,7 +268,7 @@ revert_FSE_button = Button(fse_frame, text="Revert", command=revert_FSE)
 revert_FSE_button.pack(side="right")
 
 # Create and position the frame to contain DSCP checkbox
-dscp_frame = ttk.LabelFrame(root, text="Enable DSCP settings?")
+dscp_frame = ttk.LabelFrame(root, text="Enable DSCP settings?", labelanchor='n')
 dscp_frame.pack(pady=10)
 
 # Create and position the checkbox for enabling DSCP
@@ -246,8 +276,8 @@ dscp_checkbutton = Checkbutton(dscp_frame, text="Yes", variable=enable_DSCP_var)
 dscp_checkbutton.pack(side="left")
 
 # Create and position the revert button for reverting only DSCP changes made by the script
-revert_DSCP_button = Button(root, text="Revert DSCP", command=revert_DSCP_only)
-revert_DSCP_button.pack()
+revert_DSCP_button = Button(dscp_frame, text="Revert", command=revert_DSCP_only)
+revert_DSCP_button.pack(side="right")
 
 # Modify Tkinter window setup to call on_closing function when closing
 root.protocol("WM_DELETE_WINDOW", on_closing)
